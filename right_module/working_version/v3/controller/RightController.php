85180222385d6989bf11c1fd443d7a15df1d9793
时广玉 <shiguangyu@dlaotianhuang.com>
2018-06-21 17:39:24 +0800

    2018/06/21 17:38 权限管理-v3-更新说明文件

--- right_module/working_version/v3/controller/RightController.php
+++ right_module/working_version/v3/controller/RightController.php
@@ -66,7 +66,7 @@ class RightController extends Controller
     public function rightDelRoute(Request $request)
     {
         // 获取传值
-        $index = $request->put('index ');
+        $index = $request->put('index');
         // 验证数据
         if( !$index ) return returnResponse(1,'请输入权限标识');
         // 引入Service逻辑层代码
@@ -89,7 +89,7 @@ class RightController extends Controller
     public function rightEditRoute(Request $request)
     {
         // 获取传值
-        $index      = $request->put('index ');
+        $index      = $request->put('index');
         $rightName  = $request->put('rightName');
         $rightRoute = $request->put('rightRoute');
         // 验证数据
